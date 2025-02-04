2ba1fcbcecf186150b18c1ffb2265d42d6adf0ac
TestSubjectUser <wami50731@gmail.com>
2025-02-04 18:36:14 +0530

    theme slice add

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setTheme } from "../store/themeSlice";
 import { NavLink } from "react-router";
+import ThemeToggle from "./ThemeToggle";
 
 function SideNav() {
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme.theme);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      dispatch(setTheme(savedTheme));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    document.body.className = theme; // Apply theme to body
+    localStorage.setItem("theme", theme); // Save theme to localStorage
+  }, [theme]);
+
   return (
     <div>
       <h1 className="m-10 ">LOGO</h1>
@@ -16,6 +34,7 @@ function SideNav() {
         >
           - All Entries
         </NavLink>
+        <ThemeToggle />
       </div>
     </div>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import entryReducer from "./entrySlice";
 import taskReducer from "./taskSlice";
+import themeReducer from "./themeSlice";
 
 export default configureStore({
   reducer: {
     entry: entryReducer,
     task: taskReducer,
+    theme: themeReducer,
   },
 });
